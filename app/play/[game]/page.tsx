289b1c5a001c2f61f7ceef257716f166953c0f62
Liam <n11102764@qut.edu.au>
2024-02-10 23:30:38 +1000

    fix: checked if 5 letters in guess before submitting. Stops game if guess is correct.

--- app/play/[game]/page.tsx
+++ app/play/[game]/page.tsx
@@ -20,7 +20,7 @@ export default function Game({ params }: any) {
   ]);
   const [inputValue, setInputValue] = useState("");
   const [row, setRow] = useState(0);
-
+  const [correct, setCorrect] = useState(false);
   const game = findGame(params.game);
   const answer = [game[0], game[1], game[2], game[3], game[4]];
 
@@ -48,33 +48,44 @@ export default function Game({ params }: any) {
     }
 
     if (keyPressed === "Enter") {
-      // Evaluate the guess against the answer
-      const guess = gameState[row];
-      let newGameState = [...gameState];
-      let newBackgroundColors = [...backgroundColors];
-      let backgroundColorsRow = Array(5).fill(""); // Initialize background colors for the current row
+      if (gameState[row][4] !== "") {
+        const guess = gameState[row];
+        console.log(guess);
+        console.log(answer);
+        const isCorrectGuess = guess.join("") === answer.join("");
+        console.log(isCorrectGuess);
+        if (isCorrectGuess) {
+          setCorrect(true);
 
-      guess.forEach((letter, index) => {
-        if (letter === answer[index]) {
-          // Letter found in the exact same position as the answer
-          backgroundColorsRow[index] = "bg-green-500";
-        } else if (answer.includes(letter)) {
-          // Letter found in the answer but not in the same position
-          backgroundColorsRow[index] = "bg-yellow-500";
+          // You can perform further actions if the guess is correct
         }
-      });
+        let newGameState = [...gameState];
+        let newBackgroundColors = [...backgroundColors];
+        let backgroundColorsRow = Array(5).fill(""); // Initialize background colors for the current row
 
-      // Update background colors for the current row
-      newBackgroundColors[row] = backgroundColorsRow;
+        guess.forEach((letter, index) => {
+          if (letter === answer[index]) {
+            // Letter found in the exact same position as the answer
+            backgroundColorsRow[index] = "bg-green-500";
+          } else if (answer.includes(letter)) {
+            // Letter found in the answer but not in the same position
+            backgroundColorsRow[index] = "bg-yellow-500";
+          }
+        });
 
-      // Update game state and background colors
-      setGameState(newGameState);
-      setBackgroundColors(newBackgroundColors);
+        // Update background colors for the current row
+        newBackgroundColors[row] = backgroundColorsRow;
 
-      // Move to the next row
-      setRow(row + 1);
-      setInputValue("");
-      return;
+        // Update game state and background colors
+        setGameState(newGameState);
+        setBackgroundColors(newBackgroundColors);
+
+        // Move to the next row
+        setRow(row + 1);
+        setInputValue("");
+        return;
+      }
+      // Evaluate the guess against the answer
     }
 
     if (keyPressed === "Backspace") {
@@ -104,31 +115,55 @@ export default function Game({ params }: any) {
     <>
       <Navbar />
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div className="grid grid-cols-5 grid-rows-6 gap-2">
-          {gameState.map((row, rowIndex) =>
-            row.map((cell, cellIndex) => (
-              <div key={`${rowIndex}-${cellIndex}`} className="relative">
-                {/* Background color */}
-                <div
-                  className={`absolute inset-0 opacity-50 ${backgroundColors[rowIndex][cellIndex]}`}
-                />
-                {/* Text content */}
-                <div className="flex border-2 border-gray-500 w-20 h-20 text-center uppercase font-semibold text-5xl items-center justify-center">
-                  <p>{cell}</p>
-                </div>
-              </div>
-            ))
-          )}
-        </div>
-
-        <input
-          onKeyDown={handleKeyDown}
-          value={inputValue}
-          // Update state and convert to uppercase
-          type="text"
-          className="text-white bg-[#11172a] border-2 border-gray-500 px-1 py-1 text-center uppercase"
-          maxLength={5}
-        />
+        {!correct && (
+          <>
+            <div className="grid grid-cols-5 grid-rows-6 gap-2">
+              {gameState.map((row, rowIndex) =>
+                row.map((cell, cellIndex) => (
+                  <div key={`${rowIndex}-${cellIndex}`} className="relative">
+                    {/* Background color */}
+                    <div
+                      className={`absolute inset-0 opacity-50 ${backgroundColors[rowIndex][cellIndex]}`}
+                    />
+                    {/* Text content */}
+                    <div className="flex border-2 border-gray-500 w-20 h-20 text-center uppercase font-semibold text-5xl items-center justify-center">
+                      <p>{cell}</p>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
+            <input
+              onKeyDown={handleKeyDown}
+              value={inputValue}
+              // Update state and convert to uppercase
+              type="text"
+              className="text-white bg-[#11172a] border-2 border-gray-500 px-1 py-1 text-center uppercase"
+              maxLength={5}
+            />
+          </>
+        )}
+        {correct && (
+          <div>
+            <h1>Correct!!!!</h1>
+            <div className="grid grid-cols-5 grid-rows-6 gap-2">
+              {gameState.map((row, rowIndex) =>
+                row.map((cell, cellIndex) => (
+                  <div key={`${rowIndex}-${cellIndex}`} className="relative">
+                    {/* Background color */}
+                    <div
+                      className={`absolute inset-0 opacity-50 ${backgroundColors[rowIndex][cellIndex]}`}
+                    />
+                    {/* Text content */}
+                    <div className="flex border-2 border-gray-500 w-20 h-20 text-center uppercase font-semibold text-5xl items-center justify-center">
+                      <p>{cell}</p>
+                    </div>
+                  </div>
+                ))
+              )}
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
