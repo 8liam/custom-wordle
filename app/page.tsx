b5cbc3f5c145fc29418392950e82a08c4f37b527
Liam <n11102764@qut.edu.au>
2024-02-22 14:25:02 +1000

    Made home page more friendly.

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,17 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <a className="font-3xl bg-correct p-2" href="/create">
-          Create a Quiz
+      <main className="flex min-h-screen flex-col items-center p-24 space-y-4">
+        <h1 className="text-4xl font-semibold">Customdle</h1>
+        <p className="text-xl font-light">
+          Make custom wordle games for you and your friends to challenge each
+          other.
+        </p>
+        <a
+          className="font-3xl bg-correct p-2 rounded hover:bg-wrong duration-300"
+          href="/create"
+        >
+          Create a Custom Game
         </a>
       </main>
     </>
